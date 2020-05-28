571adb8954988e535d1852ab615608875c7154da
Alex Rossell Hayes <alexander@rossellhayes.com>
2020-05-27 22:39:36 -0700

    Incorporate `trimws()` functionality in `plu_nge()`

--- R/plu_nge.R
+++ R/plu_nge.R
@@ -1,3 +1,10 @@
-plu_nge <- function(x) {
-  gsub(" +", " ", x)
+plu_nge <- function(x, ends = FALSE) {
+  x <- gsub(" +", " ", x)
+
+  if (ends) {
+    x <- gsub("^ +", "", x)
+    x <- gsub(" +$", "", x)
+  }
+
+  x
 }
