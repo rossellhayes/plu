9cfcfed6741385305492fe52232b3052caf3f486
Alex Rossell Hayes <alexander@rossellhayes.com>
2020-06-01 19:23:53 -0700

    Use unnamespaced function in tests

--- tests/testthat/test-plu_ralize.R
+++ tests/testthat/test-plu_ralize.R
@@ -1,9 +1,9 @@
 test_that("simple pluralization", {
-  expect_equal(plu::ralize("word"), "words")
+  expect_equal(plu_ralize("word"), "words")
 })
 
 test_that("character vector pluralization", {
-  expect_equal(plu::ralize(c("word", "group")), c("words", "groups"))
+  expect_equal(plu_ralize(c("word", "group")), c("words", "groups"))
 })
 
 irregs              <- c("formula", "person", "child")
@@ -14,37 +14,37 @@ irregs_conservative <- c("formulae", "people", "children")
 
 test_that("default irregular pluralization", {
   options(plu.irregulars = NULL)
-  expect_equal(plu::ralize(irregs), irregs_moderate)
+  expect_equal(plu_ralize(irregs), irregs_moderate)
 })
 
 test_that("capitalized irregular pluralization", {
   options(plu.irregulars = NULL)
   expect_equal(
-    plu::ralize(c("Formula", "Person", "Child")),
+    plu_ralize(c("Formula", "Person", "Child")),
     c("Formulas", "People", "Children")
   )
 })
 
 test_that("irregular pluralization options", {
-  expect_equal(plu::ralize(irregs, "none"), irregs_none)
-  expect_equal(plu::ralize(irregs, "liberal"), irregs_liberal)
-  expect_equal(plu::ralize(irregs, "moderate"), irregs_moderate)
-  expect_equal(plu::ralize(irregs, "conservative"), irregs_conservative)
+  expect_equal(plu_ralize(irregs, "none"), irregs_none)
+  expect_equal(plu_ralize(irregs, "liberal"), irregs_liberal)
+  expect_equal(plu_ralize(irregs, "moderate"), irregs_moderate)
+  expect_equal(plu_ralize(irregs, "conservative"), irregs_conservative)
 })
 
 test_that("options(plu.irregulars)", {
   options(plu.irregulars = "none")
-  expect_equal(plu::ralize(irregs), irregs_none)
-  expect_equal(plu::ralize(irregs, "liberal"), irregs_liberal)
+  expect_equal(plu_ralize(irregs), irregs_none)
+  expect_equal(plu_ralize(irregs, "liberal"), irregs_liberal)
   options(plu.irregulars = "liberal")
-  expect_equal(plu::ralize(irregs), irregs_liberal)
-  expect_equal(plu::ralize(irregs, "moderate"), irregs_moderate)
+  expect_equal(plu_ralize(irregs), irregs_liberal)
+  expect_equal(plu_ralize(irregs, "moderate"), irregs_moderate)
   options(plu.irregulars = "moderate")
-  expect_equal(plu::ralize(irregs), irregs_moderate)
-  expect_equal(plu::ralize(irregs, "conservative"), irregs_conservative)
+  expect_equal(plu_ralize(irregs), irregs_moderate)
+  expect_equal(plu_ralize(irregs, "conservative"), irregs_conservative)
   options(plu.irregulars = "conservative")
-  expect_equal(plu::ralize(irregs), irregs_conservative)
-  expect_equal(plu::ralize(irregs, "none"), irregs_none)
+  expect_equal(plu_ralize(irregs), irregs_conservative)
+  expect_equal(plu_ralize(irregs, "none"), irregs_none)
 })
 
 test_that("early return", {
