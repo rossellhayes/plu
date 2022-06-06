f0cad9ecbae64f7dfd911fc889d98557363ce519
Alex Rossell Hayes <alexander@rossellhayes.com>
2022-06-05 21:43:54 -0700

    chore: use `string` in `str_*()` functions

--- R/utils_str.R
+++ R/utils_str.R
@@ -1,20 +1,20 @@
-str_to_sentence <- function(x) {
-  gsub("^(.*?)(\\p{L})(.*)$", "\\1\\U\\2\\E\\3", x, perl = TRUE)
-}
-
-str_replace_all <- function(text, pattern, replacement) {
+str_replace_all <- function(string, pattern, replacement) {
   if (!is.null(names(pattern))) {
     for (i in seq_along(pattern)) {
-      text <- gsub(
+      string <- gsub(
         pattern = names(pattern)[[i]],
         replacement = pattern[[i]],
-        x = text,
+        x = string,
         perl = TRUE
       )
     }
 
-    return(text)
+    return(string)
   }
 
-  gsub(pattern = pattern, replacement = replacement, x = text, perl = TRUE)
+  gsub(pattern = pattern, replacement = replacement, x = string, perl = TRUE)
+}
+
+str_to_sentence <- function(string) {
+  sub("^(.*?)(\\p{L})(.*)$", "\\1\\U\\2\\E\\3", string, perl = TRUE)
 }
