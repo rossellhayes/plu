19fc4377ffb39db03e7c59ec331a1160b69defbe
Alex Rossell Hayes <alexander@rossellhayes.com>
2023-03-01 16:05:43 -0800

    fix(staticimports): update `stringstatic` functions to rossellhayes/stringstatic#15

--- R/staticimports.R
+++ R/staticimports.R
@@ -56,14 +56,15 @@ str_detect <- function(string, pattern, negate = FALSE) {
 }
 
 str_replace <- function(string, pattern, replacement) {
+	if (length(string) == 0 || length(pattern) == 0 || length(replacement) == 0) {
+		return(character(0))
+	}
+
 	is_fixed <- inherits(pattern, "stringr_fixed")
 
-	string <- Vectorize(sub, c("pattern", "replacement", "x"), USE.NAMES = FALSE)(
+	Vectorize(sub, c("pattern", "replacement", "x"), USE.NAMES = FALSE)(
 		pattern, replacement, x = string, perl = !is_fixed, fixed = is_fixed
 	)
-
-	mode(string) <- "character"
-	string
 }
 
 str_replace_all <- function(string, pattern, replacement) {
@@ -82,12 +83,13 @@ str_replace_all <- function(string, pattern, replacement) {
 		return(string)
 	}
 
-	string <- Vectorize(gsub, c("pattern", "replacement", "x"), USE.NAMES = FALSE)(
+	if (length(string) == 0 || length(pattern) == 0 || length(replacement) == 0) {
+		return(character(0))
+	}
+
+	Vectorize(gsub, c("pattern", "replacement", "x"), USE.NAMES = FALSE)(
 		pattern, replacement, x = string, perl = !is_fixed, fixed = is_fixed
 	)
-
-	mode(string) <- "character"
-	string
 }
 
 str_squish <- function(string) {
